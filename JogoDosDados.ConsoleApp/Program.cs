daf37dca34eca64c09021da0e29f1a4ee283917d
vicentejrmc <vicente_pc@outlook.com>
2025-03-19 15:02:53 -0300

    Add project files.

---
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace JogoDosDados.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("------ Jogo dos Dados --------");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine();
+
+                Console.Write("Aperte [ENTER] para lançar o dado...");
+                Console.ReadLine();
+
+                Random geradorDeDadoJogado = new Random();
+                int dado = geradorDeDadoJogado.Next(1, 7);
+
+                Console.WriteLine("------------------------------");
+                Console.WriteLine($"O Dado Girou! o resultado foi: {dado}");
+                Console.WriteLine("------------------------------");
+
+                Console.WriteLine("Deseja Jogar Novamente? (S/N)");
+                string continuar = (Console.ReadLine()!.ToUpper());
+                if (continuar != "S")
+                    break;    
+            }
+        }
+
+
+    }//fim da classe program
+}
+        
