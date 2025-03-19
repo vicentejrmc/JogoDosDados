b48efef00e51e74d04db1cc630becd042b20891e
vicentejrmc <vicente_pc@outlook.com>
2025-03-19 15:15:14 -0300

    Implementação de eventos epeciais: Avanço e Recuo de posições.

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -42,11 +42,33 @@ namespace JogoDosDados.ConsoleApp
                     else
                         Console.WriteLine($"Você está na posição: {posicaoJogador} de {limiteLinhaChegada}!");
 
+                    if (posicaoJogador == 5 || posicaoJogador == 10 || posicaoJogador == 15 || posicaoJogador == 25)
+                    {
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("EVENTO ESPECIAL: Avanço extra de 3 casas!");
+
+                        posicaoJogador += 3;
+
+                        Console.WriteLine($"Você avançou para a posição: {posicaoJogador}!");
+                        Console.WriteLine("----------------------------------");
+
+                    }
+                    else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20)
+                    {
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("EVENTO ESPECIAL: Recuo de 2 casas!");
+
+                        posicaoJogador -= 2;
+
+                        Console.WriteLine($"Você recuou para a posição: {posicaoJogador}!");
+                        Console.WriteLine("----------------------------------");
+                    }
+
                     Console.WriteLine("----------------------------------");
                     Console.ReadLine();
                 }
 
-                Console.Write("Deseja continuar? (s/N) ");
+                Console.Write("Deseja continuar? (S/N) ");
                 string opcaoContinuar = Console.ReadLine()!.ToUpper();
 
                 if (opcaoContinuar != "S")
